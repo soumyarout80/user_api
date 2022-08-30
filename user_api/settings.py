392c95aa011789380cefc9698d9278fd6c186d05
Soumya Ranjan Rout <soumyarout80@gmail.com>
2022-08-31 01:39:43 +0530

    Removed some unused configurations

--- user_api/settings.py
+++ user_api/settings.py
@@ -18,8 +18,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-att!dzwc3d(@b!(+r@pslkz$1nd9pwyf=9_d3%ze57%h8a+q0+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
